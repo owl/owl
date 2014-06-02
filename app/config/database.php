47f7f0674597db1f7c4e22519eea0d7b31c9bbf0
fortkle <fortkle@gmail.com>
2014-06-02 13:49:04 +0000

    データベース設定を追加

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'athena',
+			'username'  => 'athena',
+			'password'  => 'athena',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
