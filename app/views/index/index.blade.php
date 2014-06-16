c184c027e75b0fe53477ced631b30c4fa837929b
fortkle <fortkle@gmail.com>
2014-06-16 12:44:54 +0000

    トップページのレイアウトを修正

--- app/views/index/index.blade.php
+++ app/views/index/index.blade.php
@@ -41,7 +41,7 @@
     <div class="col-sm-8 blog-main">
 
       <div class="blog-post">
-        <h2 class="blog-post-title">Itemsテーブルにあるもの</h2>
+        <h2 class="blog-post-title">すべての投稿</h2>
         @foreach ($items as $item)
             <a href="{{ action('ItemController@show', $item->id) }}">{{{ $item->title }}}</a> {{ $item->created_at }}<br />
         @endforeach
@@ -57,24 +57,18 @@
 
     <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
       <div class="sidebar-module sidebar-module-inset">
-        <h4>About</h4>
-        <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+        <h4>User Info</h4>
+        <p>
+        user_id: {{$user->id}}<br />
+        email: {{$user->email}}
+        </p>
       </div>
       <div class="sidebar-module">
-        <h4>Archives</h4>
+        <h4>Menu</h4>
         <ol class="list-unstyled">
-          <li><a href="#">January 2014</a></li>
-          <li><a href="#">December 2013</a></li>
-          <li><a href="#">November 2013</a></li>
-          <li><a href="#">October 2013</a></li>
-          <li><a href="#">September 2013</a></li>
-          <li><a href="#">August 2013</a></li>
-          <li><a href="#">July 2013</a></li>
-          <li><a href="#">June 2013</a></li>
-          <li><a href="#">May 2013</a></li>
-          <li><a href="#">April 2013</a></li>
-          <li><a href="#">March 2013</a></li>
-          <li><a href="#">February 2013</a></li>
+          <li><a href="#">ホームフィード</a></li>
+          <li><a href="#">投稿テンプレート一覧</a></li>
+          <li>タグ一覧</li>
         </ol>
       </div>
       <div class="sidebar-module">
