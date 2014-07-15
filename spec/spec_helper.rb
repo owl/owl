117f23333f49d692fe2410a7073b85fdcde283a4
fortkle <fortkle@gmail.com>
2014-07-15 17:15:49 +0000

    .travis.yml修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-# 設定
+#!/bin/env ruby
 # encoding: utf-8
 require 'capybara/rspec'
 require 'capybara/poltergeist'
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,3 +1,5 @@
+#!/bin/env ruby
+# encoding: utf-8
 
 require File.dirname(__FILE__)+'/spec_helper'
 
