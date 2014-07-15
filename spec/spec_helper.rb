f4ddf13c191f876bc29be0c9df8b1266a115df06
fortkle <fortkle@gmail.com>
2014-07-15 15:48:52 +0000

    JSでエラーが起きてもCapybaraを中断しないように修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,3 +14,6 @@ Capybara.app = "athena"
 Capybara.javascript_driver = :poltergeist
 Capybara.app_host = 'http://0.0.0.0:3000'
 Capybara.run_server = false
+Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, :js_errors => false, :timeout => 60)
+end
