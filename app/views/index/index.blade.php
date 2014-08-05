4181645dd66a3fe6074b68ffafc0c0ca3dc2e50c
fortkle <fortkle@gmail.com>
2014-08-06 03:48:33 +0900

    Improve top page design

--- app/views/index/index.blade.php
+++ app/views/index/index.blade.php
@@ -14,8 +14,18 @@
                     </div>
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="/items/create">投稿する</a></li>
-                        <li><a href="/user/edit">ユーザ情報変更</a></li>
-                        <li><a href="/logout">ログアウト</a></li>
+                        <li><a href="#">ストック一覧</a></li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ HTML::gravator($User->email, 20) }}<span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="/{{{$User->username}}}">マイページ</a></li>
+                                <li><a href="/user/edit">ユーザ情報変更</a></li>
+                                <li class="divider"></li>
+                                <li><a href="/logout">ログアウト</a></li>
+                            </ul>
+                        </li>
+
                     </ul>
                 </div>
             </nav>
@@ -30,18 +40,29 @@
         <div class="container">
             <div class="row">
                 <div id="main" class="col-sm-9">
-                    <h4>すべての投稿</h4>
+                    <div class="items">
                     @foreach ($items as $item)
-                        <a href="{{ action('ItemController@show', $item->open_item_id) }}">{{{ $item->title }}}</a> {{ $item->created_at }}<br />
+                        <div class="item">
+                            {{ HTML::gravator($item->user->email, 40) }}
+                            <p><a href="/{{{$item->user->username}}}" class="username">{{{$item->user->username}}}</a>さんが<?php echo date('Y/m/d', strtotime($item->updated_at)); ?>に投稿しました。</p>
+                            <p><a href="{{ action('ItemController@show', $item->open_item_id) }}">{{{ $item->title }}}</a></p>
+                        </div>
                     @endforeach
+                    <?php echo $items->links(); ?>
+                    </div>
                 </div>
 
                 <div id="sidebar" class="col-sm-3">
-                    <ul>
-                        <li>{{ HTML::gravator($User->email) }}</li>
-                        <li>user_id: {{$User->id}}</li>
-                        <li>email: {{$User->email}}</li>
-                    </ul>
+                    <a href="/items/create" class="btn btn-success btn-block">記事を投稿する</a>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">テンプレートから作成</div>
+                        <ul class="list-group">
+                            <li class="list-group-item">日報</li>
+                            <li class="list-group-item">障害対応報告</li>
+                            <li class="list-group-item">議事録</li>
+                            <li class="list-group-item">手順書</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
