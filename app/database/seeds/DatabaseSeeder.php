e49bc4de32fd51718b3121895d6af8284006daf9
trtraki <akio55252000@yahoo.co.jp>
2014-08-12 04:16:10 +0000

    Add stock model file

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -9,15 +9,24 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Eloquent::unguard();
+            Eloquent::unguard();
 
-        DB::table('items')->truncate();
-        Item::create([
-                'title' => '1回目の投稿',
-                'body' => 'This items number is 1.',
-                'user_id' => 1,
-                'published' => 2
-        ]);
-	}
+            DB::table('items')->truncate();
+            Item::create(
+                [
+                    'title' => '1回目の投稿',
+                    'body' => 'This items number is 1.',
+                    'user_id' => 1,
+                    'published' => 2
+                ]);
+            Item::create(
+                [
+                    'title' => '2回目の投稿',
+                    'body' => 'This items number is 2.',
+                    'user_id' => 1,
+                    'published' => 2
+                ]);
 
+            $this->call('StockTableSeeder');
+        }
 }
---
+++ app/models/Stock.php
@@ -0,0 +1,5 @@
+<?php
+
+class Stock extends Eloquent {
+
+}
