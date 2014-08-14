33b8b8707f0412d99e1bd9388f0c596ad8f405c8
fortkle <fortkle@gmail.com>
2014-08-15 06:59:50 +0900

    Fix a migration settings mistake.

--- app/database/migrations/2014_08_15_065312_create_stocks.php
+++ app/database/migrations/2014_08_15_065312_create_stocks.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStocksTable extends Migration {
+class CreateStocks extends Migration {
 
 	/**
 	 * Run the migrations.
