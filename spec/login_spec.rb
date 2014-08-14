ffaab9e9e839f91eb7ca271ad8e8b168c7ff9139
fortkle <fortkle@gmail.com>
2014-08-15 08:35:32 +0900

    Add login page test case.

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -3,9 +3,31 @@
 
 require File.dirname(__FILE__)+'/spec_helper'
 
-describe 'ログインページ', :js => true do
-    it 'ログインページにアクセスする' do
-        visit "/"
+describe 'login page', :js => true do
+    it 'access login page' do
+        visit "/login"
         page.should have_content("Owl")
+    end
+end
+
+describe 'login page', :js => true do
+    it 'can login with correct user' do
+        visit "/login/"
+        fill_in('username', :with => 'admin')
+        fill_in('password', :with => 'password')
+        click_on('ログイン')
+
+        page.should have_content("投稿する")
+    end
+end
+
+describe 'login page', :js => true do
+    it 'cant login with wrong user' do
+        visit "/login/"
+        fill_in('username', :with => 'testtesttest')
+        fill_in('password', :with => 'password')
+        click_on('ログイン')
+
+        page.should have_content("ユーザ名又はパスワードが正しくありません")
     end
 end
