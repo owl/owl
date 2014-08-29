6284d7232f9c83e0a23397e09a42a0ec9641d590
hanhan1978 <ryo.tomidokoro@gmail.com>
2014-08-29 14:31:38 +0900

    refs #4 added destry

--- app/controllers/CommentController.php
+++ app/controllers/CommentController.php
@@ -6,6 +6,7 @@ class CommentController extends BaseController{
     public function create(){
         $item = Item::where('open_item_id',Input::get('open_item_id'))->first();
         $user = Sentry::findUserById(Input::get('user_id'));
+        if(preg_match("/^[\s　\t\r\n]*$/s", Input::get('body') || !$user || !$item)) return "";
         $comment = new Comment;
         $comment->item_id = $item->id;
         $comment->user_id = Input::get('user_id');
@@ -21,7 +22,8 @@ class CommentController extends BaseController{
     }
 
     public function destroy(){
-
+        $comment = Comment::find(Input::get('id'));
+        $comment->delete();
     }
 
 
--- app/views/comment/form.blade.php
+++ app/views/comment/form.blade.php
@@ -7,9 +7,9 @@
       <div style="margin-top:10px"><h4 class="media-heading" style="font-weight:bold">コメントを投稿する</h4></div>
     </div>
   </div>
-  <div style="margin-bottom:5px;">
-    {{Form::open(array('url'=>'comment/create','class'=>'form-comments', 'id'=>'comment', 'onsubmit' => 'return false;'))}}
-        {{Form::textarea('body', isset($template->body) ? $template->body : '' ,array('class'=>'form-control', 'rows'=>'5', 'id' => 'comment_text', 'placeholder' => 'コメントを入力して下さい。'))}}
+  <div style="margin-top:15px;margin-bottom:5px;">
+    {{Form::open(array('url'=>'comment/create','class'=>'form-comments', 'id'=>'comment-form', 'onsubmit' => 'return false;'))}}
+        {{Form::textarea('body', '' ,array('class'=>'form-control', 'rows'=>'5', 'id' => 'comment_text', 'placeholder' => 'コメントを入力して下さい。'))}}
         {{Form::hidden('open_item_id',$item->open_item_id)}}
         {{Form::hidden('user_id', $User->id)}}
   </div>
