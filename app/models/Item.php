cbb28c7ca158c1d2f10500ee300d9f63b71aae71
hanhan1978 <ryo.tomidokoro@gmail.com>
2014-08-30 15:11:32 +0900

    merge missed

--- app/models/Item.php
+++ app/models/Item.php
@@ -11,6 +11,7 @@ class Item extends Eloquent{
 
     public function like(){
         return $this->hasMany('Like');
+    }
 
     public function comment(){
         return $this->hasMany('Comment');
