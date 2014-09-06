49143d2fc07ccd474d60390c9a828861e4576a16
fortkle <fortkle@gmail.com>
2014-09-07 00:22:00 +0900

    Improve login page test case.

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -3,31 +3,63 @@
 
 require File.dirname(__FILE__)+'/spec_helper'
 
-describe 'login page', :js => true do
-    it 'access login page' do
-        visit "/login"
-        page.should have_content("Owl")
+describe 'Login' do
+    describe 'Smoke Test', :js => true do
+        context 'when access the top page.(not logged in)' do
+            it 'will be displayed' do
+                visit "/"
+                expect(page).to have_content 'ログイン'
+            end
+        end
+        context 'when access the top page.(logged in)' do
+            it 'will be displayed' do
+                login
+                visit "/"
+                expect(page).to have_content '投稿する'
+            end
+        end
+        context 'when access the login page.(not logged in)' do
+            it 'will be displayed' do
+                visit "/login"
+                expect(page).to have_content 'ログイン'
+            end
+        end
+        context 'when access the login page.(logged in)' do
+            it 'will be displayed' do
+                login
+                visit "/login"
+                expect(page).to have_content '投稿する'
+            end
+        end
     end
-end
 
-describe 'login page', :js => true do
-    it 'can login with correct user' do
-        visit "/login/"
-        fill_in('username', :with => 'admin')
-        fill_in('password', :with => 'password')
-        click_on('ログイン')
-
-        page.should have_content("投稿する")
-    end
-end
-
-describe 'login page', :js => true do
-    it 'cant login with wrong user' do
-        visit "/login/"
-        fill_in('username', :with => 'testtesttest')
-        fill_in('password', :with => 'password')
-        click_on('ログイン')
-
-        page.should have_content("ユーザ名又はパスワードが正しくありません")
+    describe 'validation', :js => true do
+        context 'fill in with wrong username' do
+            it 'is invalid' do
+                visit "/login/"
+                fill_in('username', :with => 'testtesttest')
+                fill_in('password', :with => 'password')
+                click_on('ログイン')
+                expect(page).to have_content 'ユーザ名又はパスワードが正しくありません'
+            end
+        end
+        context 'fill in with only username' do
+            it 'is invalid' do
+                visit "/login/"
+                fill_in('username', :with => 'testtesttest')
+                fill_in('password', :with => '')
+                click_on('ログイン')
+                expect(page).to have_content 'パスワードを入力してください'
+            end
+        end
+        context 'fill in with empty params' do
+            it 'is invalid' do
+                visit "/login/"
+                fill_in('username', :with => '')
+                fill_in('password', :with => '')
+                click_on('ログイン')
+                expect(page).to have_content 'ユーザ名とパスワードを入力してください'
+            end
+        end
     end
 end
