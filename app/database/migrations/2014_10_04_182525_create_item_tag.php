882302956fab8f327482ff93ff824f8f03be456c
fortkle <fortkle@gmail.com>
2014-10-10 08:47:22 +0900

    Add tag migration.

--- app/database/migrations/2014_10_04_182525_create_item_tag.php
+++ app/database/migrations/2014_10_04_182525_create_item_tag.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateItemsTags extends Migration {
+class CreateItemTag extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreateItemsTags extends Migration {
 	 */
 	public function up()
 	{
-        Schema::create('items_tags',function($table)
+        Schema::create('item_tag',function($table)
         {
             $table->integer('item_id')->unsigned();
             $table->foreign('item_id')->references('id')->on('items');
@@ -28,7 +28,7 @@ class CreateItemsTags extends Migration {
 	 */
 	public function down()
 	{
-        Schema::drop('items_tags');
+        Schema::drop('item_tag');
 	}
 
 }
