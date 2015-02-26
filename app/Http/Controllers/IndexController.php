b3868b9770860c2fbbc0fe11bad73f58d4257c87
fortkle <fortkle@gmail.com>
2015-02-26 11:34:45 +0900

    TOPページを表示できるように修正

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,12 +1,15 @@
 <?php namespace Owl\Http\Controllers;
 
+use Owl\Repositories\Item;
+use Owl\Repositories\Template;
+use Owl\Repositories\Stock;
+
 class IndexController extends Controller {
 
     public function index() {
-        //$items = Item::getAllItems();
-        //$templates = Template::all();
-        //$ranking_stock = Stock::getRankingWithCache(5);
-        //return View::make('index.index', compact('items', 'templates', 'ranking_stock'));
-        return view('home');
+        $items = Item::getAllItems();
+        $templates = Template::all();
+        $ranking_stock = Stock::getRankingWithCache(5);
+        return \View::make('index.index', compact('items', 'templates', 'ranking_stock', 'User'));
     }
 }
