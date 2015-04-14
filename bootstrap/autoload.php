c6a27ee303783cedfa24552fe12de914ab080328
fortkle <fortkle@gmail.com>
2015-04-14 09:14:19 +0900

    Update laravel/laravel to v5.0.22 . It needs to change $compilePath in bootstrap/autoload.php

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -27,9 +27,10 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$compiledPath = __DIR__.'/../storage/framework/compiled.php';
+$compiledPath = __DIR__.'/../vendor/compiled.php';
 
 if (file_exists($compiledPath))
 {
 	require $compiledPath;
 }
+
