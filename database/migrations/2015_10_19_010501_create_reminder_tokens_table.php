f84226d1c71456d67d2eec2649406092580ac806
fortkle <fortkle@gmail.com>
2015-10-18 16:06:47 +0000

    add: reminder_tokens table migration.

---
+++ database/migrations/2015_10_19_010501_create_reminder_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReminderTokensTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('reminder_tokens', function($table) {
+            $table->increments('id');
+            $table->integer('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->text('token')->unique();
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('reminder_tokens');
+	}
+
+}
