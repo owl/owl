5230dacf4c109a3f6f4b189e82be8cc1c40535a8
sota1235 <sota1235@gmail.com>
2015-12-27 15:21:25 +0100

    modify: add helper method for test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,18 +2,44 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
 
-		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-		return $app;
-	}
+        return $app;
+    }
 
+    /**
+     * @param $class
+     * @param $name
+     *
+     * @return \ReflectionMethod
+     */
+    protected function getProtectMethod($class, $name)
+    {
+        $class = new \ReflectionClass($class);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
+    /**
+     * @param $class
+     * @param $name
+     *
+     * @return \ReflectionProperty
+     */
+    protected function getProtectProperty($class, $name)
+    {
+        $class = new \ReflectionClass($class);
+        $property = $class->getProperty($name);
+        $property->setAccessible(true);
+        return $property;
+    }
 }
