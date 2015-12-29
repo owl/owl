714d872812a7b7ea2622179ae2b3c3c91b7b0568
sota1235 <sota1235@gmail.com>
2015-12-29 08:08:34 +0100

    modofiy: uninstall laravel-elixir

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,1 @@
-var elixir = require('laravel-elixir');
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
-    mix.less('app.less');
-});
+// TODO: make gulpfile
