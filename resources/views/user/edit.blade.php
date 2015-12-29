0c5bcaade1667d584e29fb04d1592674660aeb84
sota1235 <sota1235@gmail.com>
2015-12-29 09:47:27 +0100

    modify: add description for email notification config

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -121,29 +121,35 @@
         <h5>通知メール送信設定</h5>
     </div>
 
+    <p>
+        それぞれの項目に対してメールで通知するかどうか設定できます。
+    </p>
+
+    <br />
+
     <div class="form-group mail-checkbox-form">
-        {!! Form::label('comment-mail-checkbox', 'コメントがついた時', array('class' => 'col-sm-5 control-label mail-checkbox')) !!}
+        {!! Form::label('comment-mail-checkbox', 'コメントがついた時', array('class' => 'col-sm-4 control-label mail-checkbox')) !!}
         <div class="col-sm-4 mail-checkbox">
             {!! Form::checkbox('comment-mail-checkbox') !!}
         </div>
     </div>
     <br />
     <div class="form-group mail-checkbox-form">
-        {!! Form::label('favorite-mail-checkbox', '記事がお気に入りされた時', array('class' => 'col-sm-5 control-label mail-checkbox')) !!}
+        {!! Form::label('favorite-mail-checkbox', '記事がお気に入りされた時', array('class' => 'col-sm-4 control-label mail-checkbox')) !!}
         <div class="col-sm-4 mail-checkbox">
             {!! Form::checkbox('favorite-mail-checkbox') !!}
         </div>
     </div>
     <br />
     <div class="form-group mail-checkbox-form">
-        {!! Form::label('good-mail-checkbox', 'いいねがついた時', array('class' => 'col-sm-5 control-label mail-checkbox')) !!}
+        {!! Form::label('good-mail-checkbox', 'いいねがついた時', array('class' => 'col-sm-4 control-label mail-checkbox')) !!}
         <div class="col-sm-4 mail-checkbox">
             {!! Form::checkbox('good-mail-checkbox') !!}
         </div>
     </div>
     <br />
     <div class="form-group mail-checkbox-form">
-        {!! Form::label('edit-mail-checkbox', '自分の記事が編集された時', array('class' => 'col-sm-5 control-label mail-checkbox')) !!}
+        {!! Form::label('edit-mail-checkbox', '自分の記事が編集された時', array('class' => 'col-sm-4 control-label mail-checkbox')) !!}
         <div class="col-sm-4 mail-checkbox">
             {!! Form::checkbox('edit-mail-checkbox') !!}
         </div>
