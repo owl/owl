2ee37eb6ef0c6b6693d6ffdd68f1972c95c1d770
sota1235 <sota1235@gmail.com>
2015-12-29 08:28:50 +0100

    modify: add build script for npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
-// TODO: make gulpfile
+var gulp = require('gulp');
+
+// public/lib配下に展開したいnpmのパッケージ名
+var modules = ['bootstrap-switch'];
+
+gulp.task('export', function() {
+  modules.map(function(name, index) {
+    gulp.src('node_modules/' + name + '/**/*')
+      .pipe(gulp.dest('public/packages/' + name));
+  });
+});
+
+gulp.task('default', ['export']);
