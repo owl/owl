5e252d6eff4998619d3a9a3f2900bb20fc51d76e
sota1235 <sota1235@gmail.com>
2016-01-01 08:00:16 +0100

    add: migration file for mail notify for user #34

---
+++ database/migrations/2016_01_01_155254_create_user_mail_notifications.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserMailNotifications extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_mail_notifications', function ($table) {
+            $table->integer('user_id')->unique();
+            $table->integer('comment_notification_flag')->default(0);
+            $table->integer('favorite_notification_flag')->default(0);
+            $table->integer('good_notification_flag')->default(0);
+            $table->integer('edit_notification_flag')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_mail_notifications');
+    }
+
+}
