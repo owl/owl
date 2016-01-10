9bbd94e5b5688c955b53665d66edd1a001c6d502
sota1235 <sota1235@gmail.com>
2016-01-10 11:57:49 +0100

    modify: change button loading mode when clicked

--- public/js/like.change.js
+++ public/js/like.change.js
@@ -1,40 +1,68 @@
 $(function() {
-    $(document).on('click', '#like_id', function() {
+  /**
+   * @description 指定されたボタンをローディング中かつクリック不可にする
+   * @param {mixed} selector
+   * @return void
+   */
+  var buttonLoading = function(selector) {
+    $(selector).button('loading');
+  };
 
-        var open_id = $("#open_id").val();
-        var like_count = $("#like_count").text();
+  /**
+   * @description ボタンのローディングを解除する
+   * @param {mixed} selector
+   * @return void
+   */
+  var buttonReset = function(selector) {
+    $(selector).button('reset');
+  };
 
-        $.ajax({
-            type:"POST",
-            url:"/likes",
-            data:{"open_item_id": open_id},
+  $(document).on('click', '#like_id', function() {
+    // ボタンをローディング中にする
+    buttonLoading(this);
 
-            success: function(msg){
-            }
-        });
+    var open_id = $("#open_id").val();
+    var like_count = $("#like_count").text();
 
-        $('#like_id').html("<span class=\"glyphicon glyphicon-thumbs-up\"></span> いいね！を取り消す");
-        $('#like_id').removeClass('btn-primary');
-        $('#like_id').addClass('btn-default');
-        $('#like_id').attr('id', 'unlike_id');
-        $('#like_count').html( parseInt(like_count) + 1);
+    var ajaxPromise = $.ajax({
+      type:"POST",
+      url:"/likes",
+      data:{"open_item_id": open_id},
+
+      success: function(msg){
+      }
+    }).promise();
+
+    ajaxPromise.then(function(result) {
+      buttonReset('#like_id');
+      $('#like_id').html("<span class=\"glyphicon glyphicon-thumbs-up\"></span> いいね！を取り消す");
+      $('#like_id').removeClass('btn-primary');
+      $('#like_id').addClass('btn-default');
+      $('#like_id').attr('id', 'unlike_id');
+      $('#like_count').html( parseInt(like_count) + 1);
+    }, function(error) {
     });
-    $(document).on('click', '#unlike_id', function() {
-        var open_id = $("#open_id").val();
-        var like_count = $("#like_count").text();
-        $.ajax({
-            type:"POST",
-            url:"/likes/" + open_id,
-            data:{"_method": "delete"},
+  });
 
-            success: function(msg){
-            }
-        });
+  $(document).on('click', '#unlike_id', function() {
+    var open_id = $("#open_id").val();
+    var like_count = $("#like_count").text();
 
-        $('#unlike_id').html("<span class=\"glyphicon glyphicon-thumbs-up\"></span> いいね！");
-        $('#unlike_id').removeClass('btn-default');
-        $('#unlike_id').addClass('btn-primary');
-        $('#unlike_id').attr('id', 'like_id');
-        $('#like_count').html( parseInt(like_count) - 1);
+    var ajaxPromise = $.ajax({
+      type:"POST",
+      url:"/likes/" + open_id,
+      data:{"_method": "delete"},
+      success: function(msg){
+      }
+    }).promise();
+
+    ajaxPromise.then(function(result) {
+      $('#unlike_id').html("<span class=\"glyphicon glyphicon-thumbs-up\"></span> いいね！");
+      $('#unlike_id').removeClass('btn-default');
+      $('#unlike_id').addClass('btn-primary');
+      $('#unlike_id').attr('id', 'like_id');
+      $('#like_count').html( parseInt(like_count) - 1);
+    }, function(error) {
     });
+  });
 });
