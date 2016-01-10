f03a79a9464bf038dc973515bfb9b78de75b481e
sota1235 <sota1235@gmail.com>
2016-01-10 12:11:05 +0100

    modify: change comment button loading mode when clicked

--- public/js/comment.create.js
+++ public/js/comment.create.js
@@ -1,55 +1,93 @@
 $(function() {
-    $(document).on('submit', '#comment-form', function() {
-        $.ajax({
-            type:"POST",
-            url:"/comment/create",
-            data:{"open_item_id": this.open_item_id.value, "body": this.body.value, "_token": this._token.value},
+  /**
+   * @description 指定されたボタンをローディング中かつクリック不可にする
+   * @param {mixed} selector
+   * @return void
+   */
+  var buttonLoading = function(selector) {
+    $(selector).button('loading');
+  };
 
-            success: function(msg){
-                $('#comment-container').append(msg);
-                $('#comment-text').val("");
-            }
-        });
+  /**
+   * @description ボタンのローディングを解除する
+   * @param {mixed} selector
+   * @return void
+   */
+  var buttonReset = function(selector) {
+    $(selector).button('reset');
+  };
 
-    });
-    $(document).on('click', '.comment-delete', function() {
-        var obj = $(this).closest('div.comment'); 
-        $.ajax({
-            type:"POST",
-            url:"/comment/destroy",
-            data:{"id": obj.find('.comment-id').val(), "_token": obj.find('[name=_token]').val()},
+  $(document).on('submit', '#comment-form', function() {
+    var selector = 'input[type="submit"].item-comment-form';
+    buttonLoading(selector);
 
-            success: function(msg){
-                obj.hide();
-            }
-        });
+    var ajaxPromise = $.ajax({
+      type    : "POST",
+      url     : "/comment/create",
+      data    : {
+        "open_item_id" : this.open_item_id.value,
+        "body"         : this.body.value,
+        "_token"       : this._token.value
+      },
+      success : function(msg){
+        $('#comment-container').append(msg);
+        $('#comment-text').val("");
+      }
     });
-    $(document).on('click', '.start-edit', function() {
-        var obj = $(this).closest('div.comment'); 
-        obj.find('.right').hide();
-        obj.find('.arrow_box').hide();
-        obj.find('.comment-edit').show();
-        obj.find('.title-username').hide();
-        obj.find('.title-onedit').show();
+
+    ajaxPromise.then(function(result) {
+      buttonReset(selector);
+    }, function(error) {
     });
-    $(document).on('click', '.edit-cancel', function() {
-        var obj = $(this).closest('div.comment'); 
-        obj.find('.comment-edit').hide();
-        obj.find('.right').show();
-        obj.find('.arrow_box').show();
-        obj.find('.comment-edit-body').val(obj.find('.orig_comment').val());
-        obj.find('.title-username').show();
-        obj.find('.title-onedit').hide();
+  });
+
+  $(document).on('click', '.comment-delete', function() {
+    var obj = $(this).closest('div.comment');
+    $.ajax({
+      type    : "POST",
+      url     : "/comment/destroy",
+      data    : {
+        "id"     : obj.find('.comment-id').val(),
+        "_token" : obj.find('[name=_token]').val()
+      },
+      success : function(msg){
+        obj.hide();
+      }
     });
-    $(document).on('click', '.edit-confirm', function() {
-        var obj = $(this).closest('div.comment'); 
-        $.ajax({
-            type:"POST",
-            url:"/comment/update",
-            data:{"id": obj.find('.comment-id').val(), "body" : obj.find('.comment-edit-body').val(), "_token" : obj.find('[name=_token]').val()},
-            success: function(msg){
-                obj.html(msg);
-            }
-        });
+  });
+
+  $(document).on('click', '.start-edit', function() {
+    var obj = $(this).closest('div.comment');
+    obj.find('.right').hide();
+    obj.find('.arrow_box').hide();
+    obj.find('.comment-edit').show();
+    obj.find('.title-username').hide();
+    obj.find('.title-onedit').show();
+  });
+
+  $(document).on('click', '.edit-cancel', function() {
+    var obj = $(this).closest('div.comment');
+    obj.find('.comment-edit').hide();
+    obj.find('.right').show();
+    obj.find('.arrow_box').show();
+    obj.find('.comment-edit-body').val(obj.find('.orig_comment').val());
+    obj.find('.title-username').show();
+    obj.find('.title-onedit').hide();
+  });
+
+  $(document).on('click', '.edit-confirm', function() {
+    var obj = $(this).closest('div.comment');
+    $.ajax({
+      type    :"POST",
+      url     :"/comment/update",
+      data    : {
+        "id"     : obj.find('.comment-id').val(),
+        "body"   : obj.find('.comment-edit-body').val(),
+        "_token" : obj.find('[name=_token]').val()
+      },
+      success : function(msg){
+        obj.html(msg);
+      }
     });
+  });
 });
