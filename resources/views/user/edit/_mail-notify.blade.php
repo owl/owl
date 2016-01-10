0434eb2ed5959a4a10295fa111a4bb7c04bb1b0a
sota1235 <sota1235@gmail.com>
2016-01-10 11:07:26 +0100

    modify: remove br HTML tag, use after statement in stylesheet

--- resources/views/user/edit/_mail-notify.blade.php
+++ resources/views/user/edit/_mail-notify.blade.php
@@ -9,29 +9,27 @@
     それぞれの項目に対してメールで通知するかどうか設定できます。
 </p>
 
-<br />
-
 <div class="form-group mail-checkbox-form">
     {!! Form::label('comment-mail-checkbox', 'コメントがついた時', array('class' => 'col-sm-4 control-label mail-checkbox')) !!}
     <div class="col-sm-4 mail-checkbox">
         {!! Form::checkbox('comment-mail-checkbox', 'comment', !!$notifyFlags->comment_notification_flag) !!}
     </div>
 </div>
-<br />
+
 <div class="form-group mail-checkbox-form">
     {!! Form::label('favorite-mail-checkbox', '記事がお気に入りされた時', array('class' => 'col-sm-4 control-label mail-checkbox')) !!}
     <div class="col-sm-4 mail-checkbox">
         {!! Form::checkbox('favorite-mail-checkbox', 'favorite', !!$notifyFlags->favorite_notification_flag) !!}
     </div>
 </div>
-<br />
+
 <div class="form-group mail-checkbox-form">
     {!! Form::label('like-mail-checkbox', 'いいねがついた時', array('class' => 'col-sm-4 control-label mail-checkbox')) !!}
     <div class="col-sm-4 mail-checkbox">
         {!! Form::checkbox('like-mail-checkbox', 'like', !!$notifyFlags->like_notification_flag) !!}
     </div>
 </div>
-<br />
+
 <div class="form-group mail-checkbox-form">
     {!! Form::label('edit-mail-checkbox', '自分の記事が編集された時', array('class' => 'col-sm-4 control-label mail-checkbox')) !!}
     <div class="col-sm-4 mail-checkbox">
