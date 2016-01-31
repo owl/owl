de531eda5d9395266410f28b39dfccfadcbb5320
fortkle <fortkle@gmail.com>
2016-01-31 22:50:19 +0900

    modify: dont install 'require-dev' libraries

--- setup_app.sh
+++ setup_app.sh
@@ -4,10 +4,11 @@
 curl -sS https://getcomposer.org/installer | php
 
 # install libraries
-php composer.phar install
+php composer.phar install --no-dev
 
 # prepare files
 cp storage/database.sqlite_default storage/database.sqlite
+chmod -R 777 storage/
 
 # database migration
 php artisan migrate --seed --force
