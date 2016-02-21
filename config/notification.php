6c13fda6415c169278cf1665a59537aedca9824c
fortkle <fortkle@gmail.com>
2016-02-22 02:46:41 +0900

    #37 modify: add slack webhook config

--- config/notification.php
+++ config/notification.php
@@ -5,4 +5,11 @@ return [
      * メールによるアクション通知機能を有効にする場合はtrueを設定する
      */
     'enabled' => env('NOTIFICATION_ENABLE', false),
+
+    /**
+     * メールによるアクション通知機能を有効にする場合はtrueを設定する
+     */
+    'slack' => [
+        'webhook_url' => env('SLACK_WEBHOOK_URL', null),
+    ]
 ];
