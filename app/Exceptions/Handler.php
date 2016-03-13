ebe284aa83376cd342c0cdab18f0ada6ba08d810
sota1235 <sota1235@gmail.com>
2016-03-13 11:26:45 +0100

    modify: handling internal server error and add blade for 500

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,46 +1,83 @@
 <?php namespace Owl\Exceptions;
 
+/**
+ * @copyright (c) owl
+ */
+
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+/**
+ * Class Handler
+ *
+ * @package Owl\Exceptions
+ */
 class Handler extends ExceptionHandler {
 
-	/**
-	 * A list of the exception types that should not be reported.
-	 *
-	 * @var array
-	 */
-	protected $dontReport = [
-		'Symfony\Component\HttpKernel\Exception\HttpException'
-	];
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        'Symfony\Component\HttpKernel\Exception\HttpException'
+    ];
 
-	/**
-	 * Report or log an exception.
-	 *
-	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-	 *
-	 * @param  \Exception  $e
-	 * @return void
-	 */
-	public function report(Exception $e)
-	{
-		return parent::report($e);
-	}
-
-	/**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $e
-	 * @return \Illuminate\Http\Response
-	 */
-	public function render($request, Exception $e)
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $e
+     * @return void
+     */
+    public function report(Exception $e)
     {
-        if($e instanceof NotFoundHttpException) {
+        return parent::report($e);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $e
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $e)
+    {
+        $routeName = $this->getRouteName($request->route());
+
+        if ($e instanceof NotFoundHttpException) {
             return response()->view('errors.missing', [], 404);
         }
-		return parent::render($request, $e);
-	}
 
+        // 記事ページを参照しようとしてエラーの場合、404ページを表示
+        if ($routeName === 'items.show') {
+            return response()->view('errors.missing', [], 404);
+        }
+
+        // 本番環境の場合、500テンプレートを返す
+        if (app()->environment() === 'production') {
+            return response()->view('errors.500', [], 500);
+        }
+
+        return parent::render($request, $e);
+    }
+
+    /**
+     * Get route name from route instance safety.
+     *
+     * @param mixed  $route
+     *
+     * @return string|null
+     */
+    protected function getRouteName($route)
+    {
+        if ($route instanceof \Illuminate\Routeing\Route) {
+            return $route->getName();
+        }
+
+        return null;
+    }
 }
